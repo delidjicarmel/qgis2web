d8ee2c4c23984be49062b7e2335cf8a940472e21
Carmel K <delidjicarmel@live.fr>
2019-08-13 16:40:28 +0100

    Test

---
+++ data/CCATIC_3.js
@@ -0,0 +1,21 @@
+var json_CCATIC_3 = {
+"type": "FeatureCollection",
+"name": "CCATIC_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "N°": "1", "Départeme": "Alibori", "Commune": "Banikora", "Arrondisse": "Banikora Centre", "Latitude": 11.3004, "Longitude": 2.437 }, "geometry": { "type": "Point", "coordinates": [ 2.435899999299825, 11.371311421527286 ] } },
+{ "type": "Feature", "properties": { "N°": "2", "Départeme": "Alibori", "Commune": "Malanville", "Arrondisse": "Malanville", "Latitude": 11.8606, "Longitude": 3.3878 }, "geometry": { "type": "Point", "coordinates": [ 3.447931250332098, 11.788030171952341 ] } },
+{ "type": "Feature", "properties": { "N°": "3", "Départeme": "Atacora", "Commune": "Tanguiéta", "Arrondisse": "Tanguiéta", "Latitude": 10.6301, "Longitude": 1.2706 }, "geometry": { "type": "Point", "coordinates": [ 1.2706, 10.6301 ] } },
+{ "type": "Feature", "properties": { "N°": "4", "Départeme": "Atlantique", "Commune": "Ouidah", "Arrondisse": "Ouidah 1", "Latitude": 6.3666, "Longitude": 2.0852 }, "geometry": { "type": "Point", "coordinates": [ 2.138243748996214, 6.370686416426639 ] } },
+{ "type": "Feature", "properties": { "N°": "5", "Départeme": "Borgou", "Commune": "Nikki", "Arrondisse": "Nikki", "Latitude": 9.9363, "Longitude": 3.2084 }, "geometry": { "type": "Point", "coordinates": [ 3.281243750162077, 9.930655170057815 ] } },
+{ "type": "Feature", "properties": { "N°": "6", "Départeme": "Collines", "Commune": "Dassa-Zoumè", "Arrondisse": "Dassa-Zoumè Centre", "Latitude": 7.7815, "Longitude": 2.1836 }, "geometry": { "type": "Point", "coordinates": [ 2.257306249117659, 7.739905167823245 ] } },
+{ "type": "Feature", "properties": { "N°": "7", "Départeme": "Collines", "Commune": "Glazoué", "Arrondisse": "Glazoué", "Latitude": 8.1681, "Longitude": 2.2291 }, "geometry": { "type": "Point", "coordinates": [ 2.2291, 8.1681 ] } },
+{ "type": "Feature", "properties": { "N°": "8", "Départeme": "Couffo", "Commune": "Azovè", "Arrondisse": "Azovè", "Latitude": 6.9382, "Longitude": 1.7221 }, "geometry": { "type": "Point", "coordinates": [ 1.717998099845732, 6.944036492716037 ] } },
+{ "type": "Feature", "properties": { "N°": "9", "Départeme": "Donga", "Commune": "Copargo", "Arrondisse": "Copargo", "Latitude": 9.8382, "Longitude": 1.5462 }, "geometry": { "type": "Point", "coordinates": [ 1.542931248388995, 9.894936420021381 ] } },
+{ "type": "Feature", "properties": { "N°": "10", "Départeme": "Donga", "Commune": "Ouaké", "Arrondisse": "Ouaké", "Latitude": 9.679, "Longitude": 1.3874 }, "geometry": { "type": "Point", "coordinates": [ 1.3874, 9.679 ] } },
+{ "type": "Feature", "properties": { "N°": "11", "Départeme": "Mono", "Commune": "Grand-popo", "Arrondisse": "Agoué", "Latitude": 6.2463, "Longitude": 1.6762 }, "geometry": { "type": "Point", "coordinates": [ 1.6762, 6.2463 ] } },
+{ "type": "Feature", "properties": { "N°": "12", "Départeme": "Ouémé", "Commune": "Akpro-missérété", "Arrondisse": "Akpro-Missérété", "Latitude": 6.575, "Longitude": 2.6099 }, "geometry": { "type": "Point", "coordinates": [ 2.6099, 6.575 ] } },
+{ "type": "Feature", "properties": { "N°": "13", "Départeme": "Plateau", "Commune": "Kétou", "Arrondisse": "Kétou", "Latitude": 7.41057, "Longitude": 2.588 }, "geometry": { "type": "Point", "coordinates": [ 2.588, 7.41057 ] } },
+{ "type": "Feature", "properties": { "N°": "14", "Départeme": "Zou", "Commune": "Covè", "Arrondisse": "Cové Centre : Houèko", "Latitude": 7.2999, "Longitude": 2.2989 }, "geometry": { "type": "Point", "coordinates": [ 2.2989, 7.2999 ] } }
+]
+}
---
+++ data/PP_2.js
@@ -0,0 +1,10 @@
+var json_PP_2 = {
+"type": "FeatureCollection",
+"name": "PP_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "N°": "1", "Départeme": "Borgou", "Commune": "N'Dali", "Latitude": 9.8649, "longitude": 2.7146, "x": 2.7146, "y": 9.8649 }, "geometry": { "type": "Point", "coordinates": [ 2.614493749481991, 9.871123919997093 ] } },
+{ "type": "Feature", "properties": { "N°": "2", "Départeme": "Collines", "Commune": "Ouèssè", "Latitude": 8.49105, "longitude": 2.4225, "x": 2.4225, "y": 8.491 }, "geometry": { "type": "Point", "coordinates": [ 2.543056249409124, 8.501905168600487 ] } },
+{ "type": "Feature", "properties": { "N°": "3", "Départeme": "Zou", "Commune": "Agbangnizoun", "Latitude": 7.0737, "longitude": 1.9663, "x": 1.9663, "y": 7.0737 }, "geometry": { "type": "Point", "coordinates": [ 1.9663, 7.0737 ] } }
+]
+}
